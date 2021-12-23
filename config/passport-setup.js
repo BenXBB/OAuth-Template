5e8bb28121cd373bd53a5d9fa9f5c77107210ddd
Ben <benb803@hotmail.co.uk>
2021-12-23 19:43:35 +0000

    passport checks if user already exists in database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -8,8 +9,21 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
-    }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function after getting google account details
-        console.log(profile);
+    }, (accessToken, refreshToken, profile, done) => { // passport callback function after getting google account details.
+        // Check if user already exists on database
+        User.findOne({googleId: profile.Id}).then((currentUser) => {
+            if(currentUser) {
+                // user already in the database
+                console.log("This user currently exists...... ", currentUser);
+            } else {
+                // if not, create user in database
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => { // save is a mongoose async function used to save the user details from google
+                    console.log("new user created: " + newUser);
+                });
+            }
+        })
     })
 );
