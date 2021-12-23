f6c265a3db1ec53cd767374120239d034aed28fe
Ben <benb803@hotmail.co.uk>
2021-12-23 16:11:17 +0000

    First commit - Google route added

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+// setup view engine
+
+app.set("view engine", "ejs")
+
+//set up routes
+app.use('/auth', authRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(3000, () => {
+    console.log(`app listening for requests on port 3000`);
+});
